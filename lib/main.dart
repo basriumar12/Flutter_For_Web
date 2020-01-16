4b6436910174ba6d1e735a47703b970ffcee20e4
basri umar <basriumar12@gmail.com>
2020-01-16 16:30:26 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          textTheme: Theme.of(context).textTheme.apply(
+                fontFamily: 'Open Sans',
+              ),
+        ),
+        home: HomeView());
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: CenteredView(
+        child: Column(
+          children: <Widget>[
+            NavigationBar(),
+            Expanded(
+              child: Row(children: [
+                CourseDetails(),
+                Expanded(
+                  child: Center(
+                    child: CallToAction('Join Course'),
+                  ),
+                )
+              ]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+            width: 150,
+            child: Image.asset('assets/logo.jpg'),
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              _NavBarItem('Profil'),
+              SizedBox(
+                width: 60,
+              ),
+              _NavBarItem('About'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _NavBarItem extends StatelessWidget {
+  final String title;
+
+  const _NavBarItem(
+    this.title, {
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 18),
+    );
+  }
+}
+
+class CenteredView extends StatelessWidget {
+  final Widget child;
+
+  const CenteredView({Key key, this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 70.0, vertical: 60),
+      alignment: Alignment.topCenter,
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 1200),
+        child: child,
+      ),
+    );
+  }
+}
+
+class CourseDetails extends StatelessWidget {
+  const CourseDetails({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 600,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'Testting FLutter For WEB',
+            style: TextStyle(
+                fontWeight: FontWeight.w800, fontSize: 40, height: 0.9),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          new Container(
+            width: 400,
+            child:   Text(
+              'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys \n'
+                  ' standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+              style: TextStyle(fontSize: 15, height: 1.0),
+          )
+
+
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CallToAction extends StatelessWidget {
+  final String title;
+
+  const CallToAction(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 15),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w800,
+          color: Colors.white,
+        ),
+      ),
+      decoration: BoxDecoration(
+          color: Color.fromARGB(255, 31, 229, 146),
+          borderRadius: BorderRadius.circular(5)),
+    );
+  }
+}
